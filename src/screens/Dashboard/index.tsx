3926c95ccf0eb5c32c4e6c88ec361eb9d5df0575
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-09-13 19:06:23 -0300

    Estilização do dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { HighlightCard } from '../../Components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../Components/TransactionCard';
 
 import { 
   Container,
@@ -10,10 +12,54 @@ import {
   User,
   UserGreeting,
   UserName,
-  Icon
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps{
+  id: string;
+}
+
 export function Dashboard() {
+
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title:"Desenvolvimento de site",
+      amount:"R$ 12.000,00",
+      category:{
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date:"12/02/2021",
+    },{
+      id: '2',
+      type: 'negative',
+      title:"Hamburgueria Pizzy",
+      amount:"R$ 59,00",
+      category:{
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date:"10/04/2020",
+    },
+    { 
+      id: '3',
+      type: 'negative',
+      title:"Aluguel do apartamento",
+      amount:"R$ 1.200,00",
+      category:{
+        name: 'Casa',
+        icon: 'home'
+      },
+      date:"27/03/2021",
+    }
+  ]
+
   return (
     <Container>
       <Header>
@@ -31,7 +77,40 @@ export function Dashboard() {
           <Icon name="power"/>
         </UserWrapper>
       </Header>
-      <HighlightCard />
+
+      <HighlightCards>
+
+        <HighlightCard 
+          type="up"
+          title="Entradas"
+          amount="R$ 17.000,00"
+          lasTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lasTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard 
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lasTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+
+        
+      </Transactions>
     </Container>
   );
 }
