73f04879a970bca5d1dbed148e6c824880b49ca7
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-09-13 21:19:02 -0300

    Estilizando tela de cadastro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input } from '../../Components/Forms/Input'
+import { Button } from '../../Components/Forms/Button';
+import { TransactionTypeButton } from '../../Components/Forms/TransactionTypeButton';
 
 import { 
   Container,
   Header,
   Title,
   Form,
+  Fields,
+  TransactionTypes
 } from './styles';
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState('');
 
-  return (
-    <Container>
-        <Header>
-            <Title>Cadastro</Title>
-        </Header>
+    function handleTransactionTypeSelect(type: 'up' | 'down'){
+        setTransactionType(type);
+    }
 
-        <Form>
-            <Input 
-                placeholder="Nome"
-            />
-            <Input 
-                placeholder="Preço"
-            />
-        </Form>
-    </Container>
-  )
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input 
+                        placeholder="Nome"
+                    />
+                    <Input 
+                        placeholder="Preço"
+                    />
+
+                    <TransactionTypes>
+                        <TransactionTypeButton 
+                            type="up"
+                            title="Income"
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton 
+                            type="down"
+                            title="Outcome"
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionTypes>
+                </Fields>
+
+                <Button 
+                    title="Enviar"
+                />
+            </Form>
+        </Container>
+    )
 }
